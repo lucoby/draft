0dc3649c08773fedd516516042bccdc4260c77d5
Coby Lu <lucoby@gmail.com>
2022-01-30 17:30:49 -0500

    serve a page

--- dragndroppings/main.py
+++ dragndroppings/main.py
@@ -1,8 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+app.mount("/static", StaticFiles(directory="dragndroppings/static"), name="static")
+
+templates = Jinja2Templates(directory="dragndroppings/templates")
+
+
+@app.get("/", name="index", response_class=HTMLResponse)
+async def root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
