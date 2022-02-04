c245b8fb252219456aeab77990c89147150b84ab
Coby Lu <lucoby@gmail.com>
2022-02-04 09:40:32 -0500

    use js to add styling classes

--- dragndroppings/static/js/script.js
+++ dragndroppings/static/js/script.js
@@ -17,6 +17,7 @@ function drop(ev) {
     target.appendChild(document.getElementById(data));
 }
 
+// add drag-and-drop attributes
 document.querySelectorAll(".group-items, .item-bank").forEach(function (list) {
     list.setAttribute("ondrop", "drop(event)");
     list.setAttribute("ondragover", "allowDrop(event)");
@@ -25,4 +26,25 @@ document.querySelectorAll(".group-items, .item-bank").forEach(function (list) {
 document.querySelectorAll(".item").forEach(function (item) {
     item.setAttribute("draggable", "true");
     item.setAttribute("ondragstart", "drag(event)");
+})
+
+// add styling classes
+document.querySelectorAll(".group").forEach(function (item) {
+    item.classList.add("d-flex", "w-100", "flex-row");
+})
+
+document.querySelectorAll(".group-title").forEach(function (item) {
+    item.classList.add("d-flex", "w-25", "p-2", "border", "justify-content-center", "align-items-center");
+})
+
+document.querySelectorAll(".group-items").forEach(function (item) {
+    item.classList.add("w-100", "flex-row", "flex-wrap", "border");
+})
+
+document.querySelectorAll(".item-bank").forEach(function (item) {
+    item.classList.add("d-flex", "w-100", "flex-row", "flex-wrap");
+})
+
+document.querySelectorAll(".item").forEach(function (item) {
+    item.classList.add("d-flex", "p-2", "border", "justify-content-center", "align-items-center");
 })
