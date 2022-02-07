2969185e5ae52fdeae2267d6181ce80e73a082af
Coby Lu <lucoby@gmail.com>
2022-02-07 17:15:36 -0500

    fix text centering

--- dragndroppings/static/js/script.js
+++ dragndroppings/static/js/script.js
@@ -73,7 +73,7 @@ function render() {
     });
 
     document.querySelectorAll(".group-title").forEach(function (item) {
-        item.classList.add("w-25", "p-2", "border", "justify-content-center", "align-items-center");
+        item.classList.add("d-flex", "w-25", "p-2", "border", "justify-content-center", "align-items-center");
     });
 
     document.querySelectorAll(".group-title-name").forEach(function (item) {
@@ -124,35 +124,3 @@ function drop(ev) {
 
     render();
 }
-
-// add drag-and-drop attributes
-document.querySelectorAll(".group-items, .item-bank").forEach(function (list) {
-    list.setAttribute("ondrop", "drop(event)");
-    list.setAttribute("ondragover", "allowDrop(event)");
-})
-
-document.querySelectorAll(".item").forEach(function (item) {
-    item.setAttribute("draggable", "true");
-    item.setAttribute("ondragstart", "drag(event)");
-})
-
-// add styling classes
-document.querySelectorAll(".group").forEach(function (item) {
-    item.classList.add("d-flex", "w-100", "flex-row");
-})
-
-document.querySelectorAll(".group-title").forEach(function (item) {
-    item.classList.add("d-flex", "w-25", "p-2", "border", "justify-content-center", "align-items-center");
-})
-
-document.querySelectorAll(".group-items").forEach(function (item) {
-    item.classList.add("d-flex", "w-100", "flex-row", "flex-wrap", "border");
-})
-
-document.querySelectorAll(".item-bank").forEach(function (item) {
-    item.classList.add("d-flex", "w-100", "flex-row", "flex-wrap");
-})
-
-document.querySelectorAll(".item").forEach(function (item) {
-    item.classList.add("d-flex", "p-2", "border", "justify-content-center", "align-items-center");
-})
